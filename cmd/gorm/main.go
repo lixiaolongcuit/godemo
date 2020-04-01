a4debf20fd02f363472de25642e568e47fa1a2cf
lixiaolong <lixiaolong@liaochenyu-local.local>
2020-04-01 16:07:50 +0800

    add .gitignore; add gorm demo

---
+++ cmd/gorm/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func main() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+	db.AutoMigrate(&Product{})
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module godemo
 
 go 1.13
 
-require github.com/google/wire v0.4.0
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/google/wire v0.4.0
+	github.com/jinzhu/gorm v1.9.12
+)
